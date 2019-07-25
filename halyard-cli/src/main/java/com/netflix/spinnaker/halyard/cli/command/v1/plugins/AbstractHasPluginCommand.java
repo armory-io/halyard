781183f7d340424653f2cb1f8e09c2efe52ef31d
Cameron Motevasselani <cameron.motevasselani@armory.io>
2019-07-25 01:27:11 -0700

    feat(plugins): adding halyard commands for plugins

---
+++ halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/plugins/AbstractHasPluginCommand.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.halyard.cli.command.v1.plugins;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.netflix.spinnaker.halyard.cli.command.v1.config.AbstractConfigCommand;
+import java.util.ArrayList;
+import java.util.List;
+
+/** An abstract definition for commands that accept plugins as a main parameter */
+@Parameters(separators = "=")
+public abstract class AbstractHasPluginCommand extends AbstractConfigCommand {
+  @Parameter(description = "The name of the plugin to operate on.", arity = 1)
+  List<String> plugins = new ArrayList<>();
+
+  @Override
+  public String getMainParameter() {
+    return "plugin";
+  }
+
+  public String getPlugin(String defaultName) {
+    try {
+      return getPlugin();
+    } catch (IllegalArgumentException e) {
+      return defaultName;
+    }
+  }
+
+  public String getPlugin() {
+    switch (plugins.size()) {
+      case 0:
+        throw new IllegalArgumentException("No plugin supplied");
+      case 1:
+        return plugins.get(0);
+      default:
+        throw new IllegalArgumentException("More than one plugin supplied");
+    }
+  }
+}
---
+++ halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/plugins/EditPluginCommand.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.halyard.cli.command.v1.plugins;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.netflix.spinnaker.halyard.cli.services.v1.Daemon;
+import com.netflix.spinnaker.halyard.cli.services.v1.OperationHandler;
+import com.netflix.spinnaker.halyard.config.model.v1.plugins.Plugin;
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Parameters(separators = "=")
+public class EditPluginCommand extends AbstractHasPluginCommand {
+  @Getter(AccessLevel.PUBLIC)
+  private String commandName = "edit";
+
+  @Getter(AccessLevel.PUBLIC)
+  private String shortDescription = "Edit a plugin";
+
+  @Parameter(
+      names = "--manifest-location",
+      description = "The location of the plugin's manifest file.")
+  private String manifestLocation;
+
+  @Override
+  protected void executeThis() {
+    String currentDeployment = getCurrentDeployment();
+    String name = getPlugin();
+    Plugin plugin = new Plugin().setName(name).setManifestLocation(manifestLocation);
+
+    new OperationHandler<Void>()
+        .setFailureMesssage("Failed to edit plugin " + name + ".")
+        .setSuccessMessage("Successfully edited plugin " + name + ".")
+        .setOperation(Daemon.setPlugin(currentDeployment, name, !noValidate, plugin))
+        .get();
+  }
+}
