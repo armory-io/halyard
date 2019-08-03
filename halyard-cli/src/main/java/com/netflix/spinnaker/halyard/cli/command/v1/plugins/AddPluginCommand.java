541ea16aa906d3c3e038c3669a3baa8d877a152a
Brandon Powell <brandon.powell@armory.io>
2019-08-02 17:33:33 -0700

    chore(plugins): spotless

--- halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/plugins/AddPluginCommand.java
+++ halyard-cli/src/main/java/com/netflix/spinnaker/halyard/cli/command/v1/plugins/AddPluginCommand.java
@@ -16,17 +16,15 @@
 
 package com.netflix.spinnaker.halyard.cli.command.v1.plugins;
 
+import com.beust.jcommander.DynamicParameter;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.netflix.spinnaker.halyard.cli.services.v1.Daemon;
 import com.netflix.spinnaker.halyard.cli.services.v1.OperationHandler;
 import com.netflix.spinnaker.halyard.config.model.v1.plugins.Plugin;
+import java.util.HashMap;
 import lombok.AccessLevel;
 import lombok.Getter;
-
-import java.util.HashMap;
-import java.util.Map;
-import com.beust.jcommander.DynamicParameter;
 
 @Parameters(separators = "=")
 public class AddPluginCommand extends AbstractHasPluginCommand {
