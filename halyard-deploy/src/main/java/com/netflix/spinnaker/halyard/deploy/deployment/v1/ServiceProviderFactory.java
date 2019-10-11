2cd660a01fc16f7c7a518ab77c73875c669d3798
Nicolas Cohen <nicolas.cohen@armory.io>
2019-10-11 15:26:57 -0700

    feat(operator): Add operator type deployment - no acct

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/deployment/v1/ServiceProviderFactory.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/deployment/v1/ServiceProviderFactory.java
@@ -109,26 +109,6 @@ public class ServiceProviderFactory {
 
   private SpinnakerServiceProvider createOperatorServiceProvider(
       DeploymentConfiguration deploymentConfiguration) {
-    DeploymentEnvironment deploymentEnvironment =
-        deploymentConfiguration.getDeploymentEnvironment();
-
-    String accountName = deploymentEnvironment.getAccountName();
-
-    if (accountName == null || accountName.isEmpty()) {
-      Account account =
-          accountService.getAnyProviderAccount(deploymentConfiguration.getName(), accountName);
-      if (account != null) {
-        Provider.ProviderType providerType = ((Provider) account.getParent()).providerType();
-        if (providerType != Provider.ProviderType.KUBERNETES
-            || account.getProviderVersion() != Provider.ProviderVersion.V2) {
-          throw new HalException(
-              new ConfigProblemBuilder(
-                      Problem.Severity.FATAL,
-                      "Deployment account must be a Kubernetes v2 provider.")
-                  .build());
-        }
-      }
-    }
     return kubectlServiceProvider;
   }
 }
